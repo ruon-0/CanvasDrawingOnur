0a229b63dd4d351a845e5047a1a8eeed3bb36bb8
Onur Deniz <mail.onurdeniz@gmail.com>
2023-02-05 20:32:12 +0100

    I can draw on multiple clients now, socket receives and sends

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ var app = express();
 var server = app.listen(3000);
 app.use(express.static('public'));
 
-console.log("Wieseldiesel3000");
+
+
+console.log("My socket server is running");
 
 var socket = require('socket.io');
+
 var io = socket(server);
 
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
-    console.log('new connection' + socket.id);
+    console.log('new connection: ' + socket.id);
+
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data) {
+        socket.broadcast.emit('mouse', data);
+        console.log(data)
+    }
 }
