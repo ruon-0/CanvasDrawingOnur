de31c52bf6a9bae81366c8fed09a23968cd38a3a
Onur Deniz <mail.onurdeniz@gmail.com>
2023-02-05 20:20:14 +0100

    "drawing" included, socket configured

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,14 @@
+var socket;
+
 function setup() {
-    createCanvas(200, 200);
+    createCanvas(600, 400);
+    background(51);
+
+    socket = io.connect('http://localhost:3000');
 }
 
 function draw() {
-    background(51);
-    ellipse(mouseX, mouseY, 60, 60);
+    noStroke();
+    fill(255)
+    ellipse(mouseX, mouseY, 36, 36);
 }
--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 var server = app.listen(3000);
-
 app.use(express.static('public'));
 
 console.log("Wieseldiesel3000");
+
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log('new connection' + socket.id);
+}
